a8a1aa8265e6f2f4c07419e434b2f4150108fecb
Shakthi Jashwanth <148980747+Shaq16@users.noreply.github.com>
2025-04-06 12:55:23 +0000

    Done with this mini project

--- 11_friend_req/script.js
+++ 11_friend_req/script.js
@@ -1,6 +1,23 @@
 const request=document.querySelector("#request")
 const istatus=document.querySelector("h5")
 
+let check=0
 request.addEventListener("click",function(){
-    istatus.innerHTML="Friends"
+    if(check==0){
+        istatus.innerHTML="Friends"
+        istatus.style.color="green"
+        request.innerHTML="Remove Friend"
+        check=1;
+
+    }
+    else{
+        istatus.innerHTML="Stranger"
+        istatus.style.color="red"
+        request.innerHTML="Add Friend"
+        check=0;
+
+    }
+
+    
 })
+
