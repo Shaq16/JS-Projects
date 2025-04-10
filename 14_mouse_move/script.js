848c7c6e04d1371cc9159ab60270170d973320d0
Shakthi Jashwanth <148980747+Shaq16@users.noreply.github.com>
2025-04-10 05:59:17 +0000

    Mini Project 4

---
+++ 14_mouse_move/script.js
@@ -0,0 +1,7 @@
+const elem1=document.querySelector("#elem1");
+const elemImage = document.querySelector("#elem1 img")
+
+elem1.addEventListener("click",function(){
+    console.log("Hello");
+    
+})
