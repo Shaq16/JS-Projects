a44a6a3dc73545f28a795d2c9df18efdac3c127f
Shakthi Jashwanth <148980747+Shaq16@users.noreply.github.com>
2025-04-10 09:35:55 +0000

    Done with Mini Project 4

--- 14_mouse_move/script.js
+++ 14_mouse_move/script.js
@@ -1,7 +1,32 @@
-const elem1=document.querySelector("#elem1");
-const elemImage = document.querySelector("#elem1 img")
+const elem=document.querySelectorAll(".elem");
+const elemImage = document.querySelector(".elem img")
 
-elem1.addEventListener("click",function(){
-    console.log("Hello");
+// elem1.addEventListener("mousemove",function(e){
+//     elemImage.style.left=e.x+"px";
+//     elemImage.style.top=e.y+"px";  
+// })
+
+// elem1.addEventListener("mouseenter",function(){
+//     elemImage.style.opacity=1;
+// })
+
+// elem1.addEventListener("mouseleave",function(){
+//     elemImage.style.opacity=1;
+// })
+
+elem.forEach(function(val){ 
+
+    val.addEventListener("mouseenter",function(){
+        val.childNodes[3].style.opacity=1;
+    })
+
+    val.addEventListener("mouseleave",function(){
+        val.childNodes[3].style.opacity=0;
+    })
+
+    val.addEventListener("mousemove",function(e){
+        elemImage.style.left=e.x+"px";
+        elemImage.style.top=e.y+"px";
+    })
     
-})
+});
