888dd7a7c6701cf3ac4412d1dd24699b4752ea96
Shakthi Jashwanth <148980747+Shaq16@users.noreply.github.com>
2025-04-10 18:39:28 +0000

    Best Project till date

---
+++ 15_insta_stories/script.js
@@ -0,0 +1,26 @@
+const arr =[{dp:"https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1566437036079-0e00cc9ea796?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+            {dp:"https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1519501025264-65ba15a82390?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+            {dp:"https://images.unsplash.com/photo-1533973860717-d49dfd14cf64?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1503435824048-a799a3a84bf7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+            {dp:"https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1498855926480-d98e83099315?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+]
+
+let clutter=""
+arr.forEach(function(elem,idx){
+    clutter+=` <div class="story">
+                <img src="${elem.dp}" id="${idx}" alt="Picture">
+            </div>`
+})
+
+const stories=document.querySelector(".stories")
+stories.innerHTML=clutter
+
+
+stories.addEventListener("click",function(e){
+    document.querySelector("#full-screen").style.display="block"
+    
+    document.querySelector("#full-screen").style.backgroundImage=`url(${arr[e.target.id].story})`
+
+    setTimeout(function(){
+        document.querySelector("#full-screen").style.display="none"
+    },3000)
+})
